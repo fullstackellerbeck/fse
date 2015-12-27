cdb150e04be8521cab047c50efcf237e57b25354
fullstackellerbeck <chris@fullstackellerbeck.com>
2015-12-27 14:53:41 -0500

    Now maybe gone

--- dev/mwo/js/main.js
+++
@@ -1,9 +0,0 @@
-
-     $(function () {
-         jsKeyboard.init("virtualKeyboard");
-
-         //first input focus
-         var $firstInput = $('emailAddy').first().focus();
-         jsKeyboard.currentElement = $firstInput;
-         jsKeyboard.currentElementCursorPosition = 0;
-     });
